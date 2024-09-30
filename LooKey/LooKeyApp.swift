33f292c4d349d07f635e69e172613754e6c4b823
Mark Raj <61257131+MJRAJ01@users.noreply.github.com>
2024-09-30 18:06:27 -0400

    Initial Commit

---
+++ LooKey/LooKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooKeyApp.swift
+//  LooKey
+//
+//  Created by Mark Raj on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LooKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LooKeyTests/LooKeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LooKeyTests.swift
+//  LooKeyTests
+//
+//  Created by Mark Raj on 9/30/24.
+//
+
+import Testing
+@testable import LooKey
+
+struct LooKeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
